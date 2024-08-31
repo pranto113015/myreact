691f1477232652a903006a665da10522f1b63b55
pranto113015 <pranto3876@gmail.com>
2024-08-31 21:47:46 +0600

    practise : write jsx formate html file and show component

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Image = () => {
-    return (
-        <div>
-            <img
-      src="https://avatars.githubusercontent.com/u/50362018?v=4"
-      alt="Pranto Kumar"
-    />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hi I'm Pranto Kumar</h1>
+      <img
+        src="https://avatars.githubusercontent.com/u/50362018?v=4"
+        alt="Pranto Kumar"
+        class="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Image;
