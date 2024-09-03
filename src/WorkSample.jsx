5a52e9a1f072e30446590b9b48d2c4c9d2ba63af
pranto113015 <pranto3876@gmail.com>
2024-09-03 16:48:01 +0600

    practise : add component style like css design

--- src/WorkSample.jsx
+++ src/WorkSample.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 //passing props dynamically
 export default function WorkSample({year, experience, institute}) {
+  const mystyle = {
+    border: "1px solid red",
+    backgroundColor:"green",
+    color:"white",
+    borderRadius:"10px" ,
+    padding:"10px",
+    marginTop:"10px",
+  }
   return (
-    <div>
+    <div style={mystyle}>
       <span>{year}</span>
       <h2>{experience}</h2>
       <p>{institute}</p>
