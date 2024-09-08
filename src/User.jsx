2b279e4e2c14aac1399aa6763977c21e12ad66d2
pranto113015 <pranto3876@gmail.com>
2024-09-08 12:28:07 +0600

    practise : react click event

--- src/User.jsx
+++ src/User.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const User = ({ userInfo }) => {
+const clickHandler =(data)=>{
+console.log("Button Clicked",data)
+}
+
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
@@ -10,7 +14,7 @@ const User = ({ userInfo }) => {
         <h2 className="card-title">{userInfo.firstName}</h2>
         <p>{userInfo.email}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">{userInfo.role}</button>
+          <button onClick={()=>clickHandler(userInfo.firstName)} className="btn btn-primary">{userInfo.role}</button>
         </div>
       </div>
     </div>
