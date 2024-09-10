60ff13e95ef77fbafbb2f046acc73db19df69d9e
pranto113015 <pranto3876@gmail.com>
2024-09-10 15:23:11 +0600

    practise : Dummy Json API Load with React-Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import Button from 'react-bootstrap/Button';
-
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.users));
+  }, []);
+console.log(users)
   return (
     <>
-<Button variant='success' >Download Now</Button>
+    <div className="d-flex gap-3 flex-wrap justify-content-center">
+    {users.map((user) => (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={user.image} />
+          <Card.Body>
+            <Card.Title>{user.username}</Card.Title>
+            <Card.Text>
+             {user.university}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      ))} 
+    </div>
     </>
   );
 }
