bd4c1076b8e4a4780e50885cfe4af2c252914d8e
pranto113015 <pranto3876@gmail.com>
2024-09-12 16:06:36 +0600

    practise : react router dom install,setup and explore

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-  </>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About Page</div>,
+  },
+  {
+    path: "/service",
+    element: <div>Service Page</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
